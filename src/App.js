7f141660b5cd4c9bc27d37043d0fde841c403021
clearly-outsane <1starwarrior516@gmail.com>
2020-11-14 18:23:30 +0530

    anopther fix

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
                 <Router>
                     <Switch>
-                        <Route path='*/:id' children={<Item />} />
+                        <Route path='/item/:id' children={<Item />} />
                     </Switch>
                 </Router>
             </ThemeProvider>
